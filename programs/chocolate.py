84713b48ffacaef83a6961f76a9ff67f0f68c93b
vikas <vikas0713@gmail.com>
2015-05-28 16:34:23 +0530

    updated

--- programs/chocolate.py
+++ programs/chocolate.py
@@ -1,5 +1,18 @@
+from __future__ import division
 t=int(input())
 a=[]
 for i in range(t):
-    a.append(int(input()))
+    a.append(raw_input())
+for i in a:
+    money,price,wrap=i.split()
+    money,price,wrap=int(money),int(price),int(wrap)
+    n=money/price
+    remaining_wrapper=n/wrap
+    if remaining_wrapper==0:
+        print n+1
+    else:
+        n+=1
+        wrap+=remaining_wrapper
+        n/=n
+        
 
